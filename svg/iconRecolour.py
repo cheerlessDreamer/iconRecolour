045925a4aa0e9cbace9b9d3edbb6b2784bdd9377
Danny <danny@dannytaylor.se>
2021-08-14 17:45:08 +0200

    - Script now detects if any SVG files do not contain specified colour, and subsequently adds them to a list - No longer exports PNG file if colour doesn't match specified colour

--- svg/iconRecolour.py
+++ svg/iconRecolour.py
@@ -21,20 +21,19 @@ import glob
 import os
 import sys
 import re
-from pathlib import Path
 
 from lists import colours, sizes
 
 
-def validHexCode(hexCode):
+def validHexCode(colour):
     # CHECK IF ARGUMENT STARTS WITH A '#', ADD ONE IF NOT
-    if hexCode[0] != "#":
+    if colour[0] != "#":
         print("A hex colour must start with a '#' symbol, for example: #BADA55")
         print("Please try again.")
         return
         # TODO: Add a '#' automatically and avoid this error: 'invalid literal for int() with base 16'
 
-    if len(hexCode) < 7:
+    if len(colour) < 7:
         print("Error: Please try again with a 6-digit hex code, i.e. 'BADA55' or '#000000'")
         return
 
@@ -45,10 +44,10 @@ def validHexCode(hexCode):
     check = re.compile(regex)
 
     # COMPARE THE COMPILED REGEX TO THE STRING
-    if re.search(check, hexCode):
+    if re.search(check, colour):
         return True
     else:
-        print("Error:", hexCode, "is not a valid Hex code. Please check your input and try again.")
+        print("Error:", colour, "is not a valid Hex code. Please check your input and try again.")
         return False
 
 
@@ -59,15 +58,18 @@ def main():
 
     # CHECK IF A CUSTOM COLOUR ARGUMENT HAS BEEN PROVIDED IN THE TERMINAL
     if len(sys.argv) > 1:
-        colourToReplace = sys.argv[1].upper()
+        colourToReplaceUpper = str(sys.argv[1].upper())
+        print(colourToReplaceUpper)
+        colourToReplaceLower = str(sys.argv[1].lower())
+        print(colourToReplaceLower)
         # CHECK IF PROVIDED COLOUR IS VALID OR NOT
-        if not validHexCode(colourToReplace):
+        if not validHexCode(colourToReplaceUpper):
             return
 
     # USE DEFAULT COLOUR IF NO CUSTOM ONE IS PROVIDED
     else:
-        colourToReplace = "#00CCC5"
-        print("Using default colour: ", colourToReplace)
+        colourToReplaceUpper = "#00CCC5"
+        print("Using default colour: ", colourToReplaceUpper)
 
     # CHECK IF THERE ARE ANY SVG FILES BEFORE PROCEEDING
     if len(glob.glob("*.svg")) == 0:
@@ -78,32 +80,52 @@ def main():
         if not os.path.exists(filepath):
             os.makedirs(filepath)
 
+    filesNotExported = []
+
     # LOOP THROUGH EACH SVG FILE
     for file in glob.glob("*.svg"):
         with codecs.open(file, encoding='utf-8', errors='ignore') as svgFile:
             content = svgFile.read()
         print("Exporting:", file)
 
-        # LOOP THROUGH ALL EXPORTS (SIZE AND COLOUR)
-        for size in sizes:
-            for colour in colours:
-                newSVG = content.replace(colourToReplace, colour[1])
-                newBytes = str.encode(newSVG)
-                try:
-                    cairosvg.svg2png(bytestring=newBytes,
-                                     write_to=filepath + "/{}_{}_{}.png".format(os.path.splitext(file)[0],
-                                                                                colour[0],
-                                                                                size[0]),
-                                     output_width=size[1])
-                    totalIcons = totalIcons + 1
-                except Exception as error:
-                    print(error)
-                    print(colour)
-                pass
+        # CHECK IF FILE CONTAINS SPECIFIED COLOUR
+        if colourToReplaceUpper in content or colourToReplaceLower in content:
+
+            # LOOP THROUGH ALL EXPORTS (SIZE AND COLOUR)
+            for size in sizes:
+                for colour in colours:
+                    if colourToReplaceUpper in content:
+                        newSVG = content.replace(colourToReplaceUpper, colour[1])
+                    elif colourToReplaceLower in content:
+                        newSVG = content.replace(colourToReplaceLower, colour[1])
+
+                    newBytes = str.encode(newSVG)
+
+                    try:
+                        cairosvg.svg2png(bytestring=newBytes,
+                                         write_to=filepath + "/{}_{}_{}.png".format(os.path.splitext(file)[0],
+                                                                                    colour[0],
+                                                                                    size[0]),
+                                         output_width=size[1])
+                        totalIcons = totalIcons + 1
+                    except Exception as error:
+                        print(error)
+                        print(colour)
+                    pass
+        # IF FILE DOESN'T CONTAIN SPECIFIED COLOUR, ADD TO A LIST OF 'UNEXPORTED' FILES
+        else:
+            if file not in filesNotExported:
+                filesNotExported.append(file)
+            else:
+                continue
 
     # AT END OF RUN, GIVE INFORMATION ABOUT ICON QUANTITY
-    print("Process complete!")
-    print("Icons created: " + str(totalIcons))
+    print("\nProcess complete! Icons created: " + str(totalIcons) + "\n")
+    if len(filesNotExported) > 0:
+        print("=== CAUTION ===\nThe following files couldn't be recoloured and have not been exported: ")
+        for file in filesNotExported:
+            print("-", file)
+            print("\nCheck that the colour of these files matches the colour that you specified and try again.\n")
 
 
 if __name__ == "__main__":
